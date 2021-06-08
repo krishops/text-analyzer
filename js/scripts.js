f2a5223091187d1dad3c6fcdbcb37cfcb5b40db5
Kris Hopper <hopperdavis@gmail.com>
2021-06-08 10:54:15 -0700

    start text-analyzer project for TDD practice
    co-authored-by: Kevin Funez <kevin.funez315@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+// Business Logic
+
+// function wordCounter(text) {
+//   let wordCount = 0;
+//   const wordArray = text.split(" ");
+//   wordArray.forEach(function(word) {
+//     wordCount++;
+//   });
+//   return wordCount;
+// }
+
+function wordCounter(text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  let wordCount = 0;
+  const wordArray = text.split(" ");
+  wordArray.forEach(function(word) {
+    wordCount++;
+  });
+  return wordCount;
+}
