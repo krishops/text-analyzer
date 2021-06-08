fdd75ea3fed0757558e7e02904020423408b8940
Kris Hopper <hopperdavis@gmail.com>
2021-06-08 12:02:19 -0700

    update javascript to pass all tests

--- js/scripts.js
+++ js/scripts.js
@@ -15,8 +15,38 @@ function wordCounter(text) {
   }
   let wordCount = 0;
   const wordArray = text.split(" ");
-  wordArray.forEach(function(word) {
+  wordArray.forEach(function(element) {
+    if (!Number(element)) {
     wordCount++;
+    }
   });
   return wordCount;
 }
+
+function numberOfOccurrencesInText(word, text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  const wordArray = text.split(" ");
+  let wordCount = 0;
+  wordArray.forEach(function(element) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      wordCount++
+    }
+  });
+  return wordCount;
+}
+
+// function numberOfOccurencesInText(text, word) {
+//   if (text.trim().length === 0) {
+//     return 0;
+//   }
+//   let wordCount = 0;
+//   const wordArray = text.split(" ");
+//   wordArray.forEach(function(element) {
+//     if (!Number(element)) {
+//     wordCount++;
+//     }
+//   });
+//   return wordCount;
+// }
